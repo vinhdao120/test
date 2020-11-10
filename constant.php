21042f2e82197bd50339a6002bd53a044b3183fe
Quang Vinh <wanbituanheo@gmail.com>
2020-11-10 08:10:24 +0700

    Constan

---
+++ constant.php
@@ -0,0 +1,19 @@
+<?php
+    class User 
+    {
+        const ADMIN  = 0;
+        const CLIENT = 1;
+        const VIP1   = 2;
+        const VIP2   = 3;
+        const VIP3   = 4;
+        const VIP4   = 5;
+        const VIP5   = 6;
+    }
+
+    class News
+    {
+        const SAAG = 1;
+        const SOSP = 2;
+        const SOUP = 3;
+        const SOI = 4;
+    }
